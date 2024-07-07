556ead92cc10dd8acac8a2c4575bbceeb0d37cbb
Ceejay <osadebec98@gmail.com>
2024-07-07 06:38:34 +0100

    Initial

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,17 @@
+# arithmetic_operations.py
+
+def perform_operation(num1, num2, operation):
+    match operation:
+        case 'add':
+            return num1 + num2
+        case 'subtract':
+            return num1 - num2
+        case 'multiply':
+            return num1 * num2
+        case 'divide':
+            if num2 == 0:
+                return "Error: Division by zero is not allowed."
+            else:
+                return num1 / num2
+        case _:
+            return "Invalid operation. Please choose add, subtract, multiply, or divide."
